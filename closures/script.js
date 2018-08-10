761e2e6580736906513a677dbc04cb8f088ef075
zeyuri <gipsyonacid@gmail.com>
2018-08-10 12:16:35 -0300

    closure exercises before correction

---
+++ closures/script.js
@@ -0,0 +1,40 @@
+// 1-4
+var nonsense = function(string) {
+    var blab = function(){
+        alert(string);
+    }
+    return blab;
+}
+
+var blabLater = nonsense('foo');
+var blabAgainLater = nonsense('bar');
+
+blabLater();
+blabAgainLater();
+
+// 5
+var lastNameTrier = function(firstName){ 
+     var innerFunction = function(lastName) { 
+         console.log(firstName + ' ' + lastName)
+     };
+     return innerFunction;
+ };
+ var firstNameFarmer = lastNameTrier('Farmer'); //logs nothing
+ firstNameFarmer('Brown'); //logs 'Farmer Brown'
+ 
+firstNameFarmer('Jane'); //logs 'Farmer Jane'
+firstNameFarmer('Lynne');
+
+// 6
+
+var storyWriter = function() {
+    var story = '';
+    return {
+        addWords: function(string){
+            return story = story + string;
+        },
+        erase: function(){
+            return story = '';
+        }
+    }
+}
